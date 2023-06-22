c198ee4fca925fe62917da66ffdbb55e0f6be386
karasew15 <karasew1599@mail.ru>
2023-06-22 20:40:08 +0400

    Initial Commit

---
+++ Домашнее задание №10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Домашнее задание №10
+//
+//  Created by Сергей Карасев on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
